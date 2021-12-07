9bfb9984a1ab266c07cec111a36b8298747ac6bf
Josh Ghiloni <jghiloni@vmware.com>
2021-12-07 07:25:59 -0500

    day 7 exercise 1 attempt 1

--- days/day7.go
+++ days/day7.go
@@ -1,11 +1,41 @@
 package days
 
+import "fmt"
+
 type day7 struct{}
 
 func (d *day7) Part1() string {
-	return ""
+	// initial theory based on example input: normalizing to the mode provides
+	// the optimal fuel consumption as it provides the most zeroes
+
+	crabPositions := slurpListAsInts(input7)
+	normalPosition := mode(crabPositions)
+
+	fuelTotal := 0
+	for _, pos := range crabPositions {
+		fuelTotal += intAbs(pos - normalPosition)
+	}
+
+	return fmt.Sprintf("mode = %d, fuel total = %d", normalPosition, fuelTotal)
 }
 
 func (d *day7) Part2() string {
 	return ""
+}
+
+func mode(list []int) int {
+	set := map[int]int{}
+
+	for _, i := range list {
+		set[i]++
+	}
+
+	max := 0
+	for k := range set {
+		if set[k] > set[max] {
+			max = k
+		}
+	}
+
+	return max
 }
